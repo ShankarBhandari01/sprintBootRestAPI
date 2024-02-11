75e796c4d552bd0d9341f98ab9221732deffb566
shankar bhandari <harryshankar@outlook.com>
2024-02-11 14:49:33 +0545

    Initial commit

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.JULY;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student shanakr = new Student(
+                    "Shankar",
+                    LocalDate.of(2000, JULY, 31),
+                    "sankar@gmail.com");
+
+            Student bhandari = new Student(
+                    "bhandari",
+                    LocalDate.of(2004, JULY, 31),
+                    "bhandari@gmail.com");
+            studentRepository.saveAll(
+                    List.of(shanakr, bhandari)
+            );
+        };
+    }
+
+}
